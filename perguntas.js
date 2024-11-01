713a900165aae15f707f7e03d6b936505c6958cd
Adriano0708 <adriano.didi070806@gmail.com>
2024-10-31 22:24:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'Quais são as principais características do Python?',
+    'O Python é conhecido por ser uma linguagem de programação de alto nível, fácil de ler e com uma grande biblioteca padrão.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país conhecido como a terra dos faraós?',
+    'O país conhecido como a terra dos faraós é o Egito.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos ou instruções para resolver um problema ou realizar uma tarefa.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Qual é a tradução de "bom dia" em inglês?',
+    '"Bom dia" em inglês é "good morning".'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa "variável" em programação?',
+    'Uma variável é um espaço de armazenamento que pode conter dados e cujo valor pode ser alterado durante a execução do programa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o continente onde está localizado o Brasil?',
+    'O Brasil está localizado na América do Sul.'
+)
